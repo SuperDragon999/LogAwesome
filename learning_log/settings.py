c4ae043a4dbe9e92bd94ee36af970b383094c37d
JJ1234 <JJ1234@example.com>
2022-10-07 15:44:39 +0800

    Fixed collectstatic problem

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zi$+5n6wqwh-9p)!)qrpz%j!7inqiv-fgsf&$8s0zhz26@+pdc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 #TODO: This thing is fine. BUT DANGEROUS. Find another safer option
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', 'logawesome.herokuapp.com']
 
 if os.environ.get('DEBUG') == 'FALSE':
     DEBUG = False
@@ -55,12 +55,20 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'build/assets')
 ]
 
 ROOT_URLCONF = 'learning_log.urls'
@@ -135,3 +143,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_heroku
+django_heroku.settings(locals())
